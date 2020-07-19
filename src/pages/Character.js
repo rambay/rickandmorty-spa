cea6743239cbf19927cc829c5b673827a110b7d0
Diego Ramirez <rambay1990@gmail.com>
2020-07-18 22:19:07 -0500

    first commit single page app

---
+++ src/pages/Character.js
@@ -0,0 +1,27 @@
+import getHash from "../utils/getHash";
+import getData from "../utils/getData";
+
+const Character = async () => {
+  const id = getHash();
+  const character = await getData(id);
+  const { episode, status, species, gender, origin, location } = character;
+  const view = `
+    <div class="Characters-inner">
+      <article class="Characters-card">
+        <img src="${character.image}" alt="${character.name}">
+        <h2>${character.name}</h2>
+      </article>
+      <article class="Characters-card">
+        <h3>Episodes: <span>${episode.length}</span></h3>
+        <h3>Status: <span>${status}</span></h3>
+        <h3>Species: <span>${species}</span></h3>
+        <h3>Gender: <span>${gender}</span></h3>
+        <h3>Origin: <span>${origin.name}</span></h3>
+        <h3>Last Location: <span>${location.name}</span></h3>
+      </article>
+    </div>
+  `;
+  return view;
+};
+
+export default Character;
